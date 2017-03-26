ad249d09216d7b670433d718ef454851b39464cf
InnOpyU <innopyu@gmail.com>
2017-03-26 22:50:20 +0800

    LAB04 part 1 first commit

---
+++ src/adderapp/domain/Adder.java
@@ -0,0 +1,22 @@
+package adderapp.domain;
+
+public class Adder {
+	private static final int INITIAL_VALUE = 0; // constant
+	private int total;
+
+	public Adder() {
+		reset();
+	}
+
+	public void reset() {
+		total = INITIAL_VALUE;
+	}
+
+	public void addToTotal(int operand) {
+		total = total + operand;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+}
